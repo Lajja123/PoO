697713d9b423e026f413e255dfa0f4c676a1f605
Lajja <lajja.lamprostech@gmail.com>
2022-11-05 14:28:55 +0530

    PoO project 2:28pm

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -77,42 +77,63 @@ function LandingPage() {
   };
   return (
     <>
-      <section className="p-main-container">
-        <div className="p-navbar-main">
-          <button onClick={connectWallet} className="p-connect-btn">
-            <Link to="/register">
-              {walletAddress && walletAddress.length > 0
-                ? `Connected: ${walletAddress.substring(
-                    0,
-                    6
-                  )}...${walletAddress.substring(38)}`
-                : " Connect Wallet"}
-            </Link>
-          </button>
-          <img src={topwave} alt="waves" className="p-top1-waves" />
-        </div>
-        <div className="d-hero">
-          <video autoPlay loop muted className="p-middle-video">
-            <source src={nftvideo} type="video/mp4" />
-            <source src={nftvideo} type="video/ogg" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-
-        <section className="p-hero-main">
-          <section className="p-landingpage-section2">
+      <section className="poo-main-section">
+        <div className="poo-section1-landingpage">
+          <div className="p-navbar-main">
+            <button
+              onClick={connectWallet}
+              className="p-connect-btn font-face-gm-aqiure"
+            >
+              <Link to="/register">
+                {walletAddress && walletAddress.length > 0
+                  ? `Connected: ${walletAddress.substring(
+                      0,
+                      6
+                    )}...${walletAddress.substring(38)}`
+                  : " Connect Wallet"}
+              </Link>
+            </button>
+          </div>
+          <div className="p-wave1-main">
+            <img src={topwave} alt="waves" className="p-top1-waves" />
+          </div>
+          <div className="d-hero">
+            <video autoPlay loop muted className="p-middle-video">
+              <source src={nftvideo} type="video/mp4" />
+              <source src={nftvideo} type="video/ogg" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="p-wave2-main">
             <img src={topwave} alt="waves" className="p-top2-waves" />
+          </div>
+        </div>
 
+        <section className="p-section2-landingpage">
+          <div className="section2-main-div">
             <div className="p-nft-content font-face-gm">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              <h3>"Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
             </div>
-            <img src={circle1} alt="" className="p-circle1" />
             <div className="temp-svg">
-              <img src={circle1} alt="" className="p-circle2" />
+              <div className="p-circle1-svg">
+                <img src={circle1} alt="" className="p-circle1" />
+              </div>
+              <div className="p-circle2-svg">
+                <img src={circle1} alt="" className="p-circle2" />
+              </div>
             </div>
-            <img src={abort1} alt="abort" className="p-abort-wave" />
-            <img src={circle1} alt="" className="p-circle3" />
-            <img src={circle1} alt="" className="p-circle4" />
+
+            <div className="p-abortwave-main ">
+              <img src={abort1} alt="abort" className="p-abort-wave" />
+            </div>
+            <div className="temp-svg2">
+              <div className="p-circle3-svg">
+                <img src={circle1} alt="" className="p-circle3" />
+              </div>
+              <div className="p-circle4-svg">
+                <img src={circle1} alt="" className="p-circle4" />
+              </div>
+            </div>
 
             <div className="p-nfts-main">
               <img src={img1} alt="" className="nfts p-img1" />
@@ -124,26 +145,35 @@ function LandingPage() {
               <img src={img7} alt="" className="nfts p-img7" />
               <img src={img8} alt="" className="nfts p-img8" />
             </div>
-          </section>
-          <section className="p-landingpage-section3"></section>
-          <img src={coin} alt="" className="p-nft-coin" />
-
-          <div className="p-circle-main">
-            <img src={circle1} alt="" className="p-circle-5" />
           </div>
-          <div className="p-nft-certi-main-poo">
-            <img src={certificate} alt="" className="p-nft-certificate" />
-            <img src={certiImg} alt="" className="p-nft-certificateImage" />
-            <div className="certi-title font-face-gm-extralight ">
-              Certificate Of Ownership
+        </section>
+        <section className="p-section3-landingpage">
+          <div className="section3-main-div">
+            <div className="coin-circle-flex">
+              <div className="p-coin-main">
+                <img src={coin} alt="" className="p-nft-coin" />
+              </div>
+              <div className="p-circle-main">
+                <img src={circle1} alt="" className="p-circle-5" />
+              </div>
             </div>
-            <div className="p-certi-content">
-              <div className="font-face-gm">Nft was verified on</div>
-              <div className="font-face-gm">Verified By</div>
-            </div>
-            <div className="p-certi-content2">
-              <div className="font-face-gm-thin">17/10/2022</div>
-              <img src={verify} alt="" className="p-nft-verifify" />
+            <div className="p-nft-certi-main-poo">
+              <img src={certificate} alt="" className="p-nft-certificate" />
+
+              <div className="img-content-flex">
+                <img src={certiImg} alt="" className="p-nft-certificateImage" />
+                <div className="certi-title font-face-gm-extralight ">
+                  Certificate Of Ownership
+                </div>
+                <div className="p-certi-content">
+                  <div className="font-face-gm">Nft was verified on</div>
+                  <div className="font-face-gm">Verified By</div>
+                </div>
+                <div className="p-certi-content2">
+                  <div className="font-face-gm-thin">17/10/2022</div>
+                  <img src={verify} alt="" className="p-nft-verifify" />
+                </div>
+              </div>
             </div>
           </div>
         </section>
