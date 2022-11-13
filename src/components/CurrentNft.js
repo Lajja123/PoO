4d42b22707c48bfd404585ea4544b4f1caf1ec38
Lajja <lajja.lamprostech@gmail.com>
2022-11-13 19:25:16 +0530

    changes in chain name

--- src/components/CurrentNft.js
+++ src/components/CurrentNft.js
@@ -146,7 +146,7 @@ function CurrentNft() {
                 <option value="select">select</option>
                 <option value="eth">eth</option>
                 <option value="0x1">0x1</option>
-                <option value="goerli">goerlli</option>
+                <option value="goerli">goerli</option>
                 <option value="0x5">0x5</option>
                 <option value="sepolia">sepolia</option>
                 <option value="0xaa36a7">0xaa36a7</option>
